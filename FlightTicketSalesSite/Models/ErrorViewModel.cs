ae755cf96aef47649d78b523d7ce2684cc50a26c
ismail ucar2 <iucar2@32bit.com.tr>
2023-10-26 01:36:53 +0300

    first commit

---
+++ FlightTicketSalesSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlightTicketSalesSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
